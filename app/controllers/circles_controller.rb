dadd532f788339b33624d9fc0e98fc82013e5731
dec <dec123@dec-no-MacBook-Pro.local>
2016-03-11 15:34:36 +0900

    サークル、タグ、大学、リンク整備完了

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -6,6 +6,8 @@ class CirclesController < ApplicationController
 
   def search
     @circles = Circle.where('name LIKE(?)', "%#{params[:search]}%")
+    @univs = Univ.all
+    @tags = Tag.all
   end
 
   def edit
