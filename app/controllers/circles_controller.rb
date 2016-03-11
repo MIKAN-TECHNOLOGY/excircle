7d75bc0521fe30eb6c8e57999043965b7f35b470
dec <dec123@dec-no-MacBook-Pro.local>
2016-03-11 19:06:25 +0900

    サークル詳細ページ作成

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -2,6 +2,9 @@ class CirclesController < ApplicationController
   def show
     @circles = Circle.where(id: params[:id])
     @events = Event.where(circle_id: @circles)
+    @user_id=Circle.where(id: params[:id]).select("user_id")
+    @univ_id=User.where(id: @user_id).select("univ_id")
+    @thisuniv=Univ.where(id: @univ_id)
     @univs = Univ.all
     @tags = Tag.all
   end
