d749e3c3fcedc3da00ed34b9ba8bfc7de46a0828
KT <x27c5343bd78n@gmail.com>
2016-03-13 00:26:47 +0900

    微調整

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -1,4 +1,9 @@
 class Circle < ActiveRecord::Base
+  has_attached_file :avatar,
+                      styles:  { medium: "300x300#", thumb: "100x100#" }
+  validates_attachment_content_type :avatar,
+                                      content_type: ["image/jpg","image/jpeg","image/png"]
+
   belongs_to :user
   belongs_to :tag
   has_many :events
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
+  has_attached_file :avatar,
+                      styles:  { medium: "300x300#", thumb: "100x100#" }
+  validates_attachment_content_type :avatar,
+                                      content_type: ["image/jpg","image/jpeg","image/png"]
+
   belongs_to :circle
 end
