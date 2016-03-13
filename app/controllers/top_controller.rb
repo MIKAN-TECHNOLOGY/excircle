2672e3a34bc1a655b671455b66f5dda75731af35
KT <x27c5343bd78n@gmail.com>
2016-03-13 19:23:51 +0900

    ページネーションと新着機能

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,8 @@
 class TopController < ApplicationController
   def index
+    @circles = Circle.limit(6).order('id DESC')
+    @events = Event.limit(6).order('id DESC')
+    @users = User.all
     @univs = Univ.all
     @tags = Tag.all
   end
